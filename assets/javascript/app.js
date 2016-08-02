d1c233dc54e470271b99e63516e4bf195c679bfd
JenPhD <jpemberton76@gmail.com>
2016-08-02 18:31:06 -0400

    buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,33 @@
 $(document).ready(function () {
-console.log("sanity check");
+
+
+//Initial array of bands and artists
+var bands = ['The Talking Heads', 'George Clinton', 'The Beatles', 'Gorrilaz', 'Prince'];
+
+//Generic function for displaying band data
+function renderButtons () {
+	$('#btns').empty();
+
+	for(var i = 0; i < bands.length; i++) {
+
+		var btn = $('<button>')
+
+		btn.addClass('btn btn-default btn-lg');
+		btn.attr('data-name', bands[i]);
+		btn.text(bands[i]);
+		$('#btns').append(btn);
+	}	
+
+}
+
+renderButtons();
+
+//creating query URL
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=music&limit=9&api_key=dc6zaTOxFJmzC";
+
+	$.ajax({url: queryURL, method: 'GET'})
+	 .done(function(response) {
+	     console.log(response);
+	}); 
+
 });//ending document ready.
