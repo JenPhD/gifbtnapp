eb334eb9474854cba0dbeb491ac6d1b10ea4c5ed
JenPhD <jpemberton76@gmail.com>
2016-08-04 13:00:37 -0400

    got the gifs going

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,94 +1,109 @@
-//SETUP VARIABLES
-//================================================================================
-var authKey = "dc6zaTOxFJmzC";
-var queryURLBase = "http://api.giphy.com/v1/gifs/search?q=" 
-//These variables wil hold the results we get from the user input after
-//adding band buttons and clicking on buttons
+$(document).ready(function () {
 
-//Initial array of bands and artists
-var bands = ['The Talking Heads', 'George Clinton', 'The Beatles', 'Gorrilaz', 'Prince'];
+	//SETUP VARIABLES
+	//================================================================================
+	var authKey = "dc6zaTOxFJmzC";
+	var queryURLBase = "http://api.giphy.com/v1/gifs/search?q=" 
+	//These variables wil hold the results we get from the user input after
+	//adding band buttons and clicking on buttons
 
-//FUNCTIONS
-//==================================================================================
+	//Initial array of bands and artists
+	var bands = ['The Talking Heads', 'George Clinton', 'The Beatles', 'Gorrilaz', 'Prince'];
+	
+	//FUNCTIONS
+	//==================================================================================
 
-
-
-//First load the initial array of bands.
+	//First load the initial array of bands.
 	function renderButtons () {
 		$('#btns').empty();
 
 		//for loop for initial group of buttons
 		for(var i = 0; i < bands.length; i++) {
 
-			var btn = $('<button>')
-				btn.addClass('btn btn-default btn-lg');
+			var btn = $('<button>');
+				btn.addClass('btn btn-default btn-lg music');
 				btn.attr('data-name', bands[i]);
 				btn.text(bands[i]);
 				$('#btns').append(btn);
 
-
-	// This function handles events when the user inputs a band and clicks on add band
+		}
+	}
+	renderButtons();
+			
+	// Event listener for added bands. 
+	//This function handles events when the user inputs a band and clicks on add band
 
 	$('#addBand').on('click', function(){
 
-	// This line of code will grab the input from the textbox
-	var band = $('#band-input').val().trim();
+		// This line of code will grab the input from the textbox and trim any extra spaces
+		var band = $('#band-input').val().trim();
+		//console.log(band);
 
-	// The movie from the textbox is then added to our array
-	bands.push(band);
+		// The movie from the textbox is then added to our array
+		bands.push(band);
 		
-	// Our array then runs which handles the processing of our movie array
-	renderButtons();
+		// Our array then runs again to add the user input buttons
+		renderButtons();
 
-	// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
-	return false;
+		// We have this line so that users can hit "enter" instead of clicking on the button
+		return false;
 	});
 
+	// =======================================================
+	//Create the queryURL, which equals the base qrl + the band name for the search term. You get 
+	//the band name from an on click event listener. Used document.body because the music class was 
+	//created from the function above and was thus out of scope just for a generic on click function.
+	$(document.body).on('click', '.music', function() {
+		
+		//This replaces the spaces in a band name with + so the URL works
+		var searchTerm = $(this).data('name').split(' ').join('+');
 
-// =======================================================
-//Create the queryURL, which equals the base qrl + the band name for the search term. You get 
-//the band name from an on click event listener.
-$('#btns').on('click', function() {
+		//This builds the queryURL with the queryURLBase, the search term, a limit of 9 gifs,
+		//and the authorization key. 
+		var queryURL =  queryURLBase + searchTerm + "&limit=9&api_key=" + authKey;
+		console.log(queryURL);
 
-	var searchTerm = $(this).attr('data-name');
+		//Retrieves the JSON from the giphy API.
+		$.ajax({url: queryURL, method: 'GET'})
+	 	.done(function(response) {
+	    console.log(response);
 
-	
+	    	//Giphys is an array of objects returned from the API.
+	    	var giphys = response.data;
+	    	console.log(giphys);
 
-	queryURL = queryURLBase + searchTerm + "&limit=9&api-key=" + authKey;
-	console.log(queryURL);
+	    	//Count of gifs displayed with each click of the button
+			//imgCount = 0;
+
+	    		//for loop for the 9 objects in the giphy array.
+	    		for (var j = 0; j < giphys.length; j++) {
+	     	
+	     			//first empty the div with the gifs
+	     			//$('#gifrow').empty();
+
+	     			//Create a new div for the music gifs
+
+	     			var musicDiv = $('<div class="gif">');
+
+	     			//Add the ratings
+	      			var rating = $('<p>').text("Rating: " + giphys[j].rating);
+	     			//console.log(rating);
+
+	     			var bandImages = $('<img>');
+	     			bandImages.attr('src', giphys[j].images.fixed_height.url);
+
+	     			musicDiv.append(rating);
+	     			musicDiv.prepend(bandImages);
+
+	     			$('#gifrow').prepend(musicDiv);
+
+	     			//imgCount++;
+	     			//console.log(imgCount);
+	      		}
+	      	
+		});
 	});
-
-
-//Function that runs the query with the clicked on band
-		//creating query URL
-		//var queryURL = "http://api.giphy.com/v1/gifs/search?q=The Talking Heads&limit=9&api_key=dc6zaTOxFJmzC";
-
-	// function getGifs (queryURL){
-	// 	$.ajax({url: queryURL, method: 'GET'})
-	//  		.done(function(response) {
-	//      	console.log(response);
-	// 		//creating a variable for the ratings of the gifs
-	//      	var rating = response.data[i].rating;
-	//      	console.log(rating);
-
-	//      	//Setting the variable of image-url to the specific object
-	//      	var imageUrl = response.data[i].image_original_url;
-
-	//      	//Creating a variable called bandImage 
-	//      	var bandImage = $("<img>");
-
-	//      	//Giving the bandImage the source path and an alt tag
-	//      	bandImage.attr('src', imageUrl);
-	//      	bandImage.attr('alt', 'btn.text(bands[i])');
-
-	//      	//Adding images
-
-	//      	$('#gifrow').append(bandImage);
-	// 	}); 
-	// }
-	}
-}
-renderButtons();
+});//ending document ready
 	
 
 
